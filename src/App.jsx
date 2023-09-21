bddc81b66c2bfbd927d5a9e265ff8ba6f66d0e2b
RafatSh-hash <rafatsh800@gmail.com>
2023-09-21 20:26:22 +0600

    added errorComponent

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,29 @@ import Main from "./Main/Main";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/SignUp/SignUp";
+import Errorpage from "./Components/Errorpage/Errorpage";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <Errorpage></Errorpage>,
       children: [
         {
           path: "/",
           element: <Home></Home>,
+          errorElement: <Errorpage></Errorpage>,
         },
         {
           path: "/login",
           element: <Login></Login>,
+          errorElement: <Errorpage></Errorpage>,
         },
         {
           path: "/signup",
           element: <SignUp></SignUp>,
+          errorElement: <Errorpage></Errorpage>,
         },
       ],
     },
---
+++ src/Components/Errorpage/Errorpage.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Errorpage = () => {
+  return (
+    <div>
+      <h1>This is Error Page</h1>
+    </div>
+  );
+};
+
+export default Errorpage;
